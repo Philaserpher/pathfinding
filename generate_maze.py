ff48174a9e1158193b1bcba0d74d47f2587b1022
Philaserpher <jpfgd20@gmail.com>
2023-03-28 23:40:27 +0100

    asd

--- generate_maze.py
+++ generate_maze.py
@@ -12,8 +12,10 @@ def generate_maze(grid, rows):
         for node in row:
             node.set_wall()
 
-    pos = (random.randint(1, (rows-2)/2)*2-1,
-           random.randint(1, (rows-2)/2)*2-1)
+    pos = (
+        random.randint(1, (rows - 2) / 2) * 2 - 1,
+        random.randint(1, (rows - 2) / 2) * 2 - 1,
+    )
 
     node = grid[pos[0]][pos[1]]
     node.reset()  # choose a random position, and clear the node
@@ -34,7 +36,7 @@ def generate_maze(grid, rows):
     start.set_start()
     end = grid[-3][-3]
     end.set_end()  # set start and ends on opposite corners
-    return(grid, start, end)
+    return (grid, start, end)
 
 
 def get_neighbour(pos, grid, rows):  # used to randomly select neighbours
@@ -45,22 +47,28 @@ def get_neighbour(pos, grid, rows):  # used to randomly select neighbours
     while True:
 
         key = sides[key]
-        if (key == 1 and pos[1] > 2 and
-                not grid[pos[0]][pos[1]-2].is_empty()):
-            grid[pos[0]][pos[1]-1].reset()
-            return((pos[0], pos[1]-2))
-        elif (key == 2 and pos[0] < rows-3 and
-              not grid[pos[0]+2][pos[1]].is_empty()):
-            grid[pos[0]+1][pos[1]].reset()
-            return((pos[0]+2, pos[1]))
-        elif (key == 3 and pos[1] < rows-3 and
-              not grid[pos[0]][pos[1]+2].is_empty()):
-            grid[pos[0]][pos[1]+1].reset()
-            return((pos[0], pos[1]+2))
-        elif (key == 4 and pos[0] > 2 and
-              not grid[pos[0]-2][pos[1]].is_empty()):
-            grid[pos[0]-1][pos[1]].reset()
-            return((pos[0]-2, pos[1]))
+        if key == 1 and pos[1] > 2 and not grid[pos[0]][pos[1] - 2].is_empty():
+            grid[pos[0]][pos[1] - 1].reset()
+            return (pos[0], pos[1] - 2)
+        elif (
+            key == 2
+            and pos[0] < rows - 3
+            and not grid[pos[0] + 2][pos[1]].is_empty()
+        ):
+            grid[pos[0] + 1][pos[1]].reset()
+            return (pos[0] + 2, pos[1])
+        elif (
+            key == 3
+            and pos[1] < rows - 3
+            and not grid[pos[0]][pos[1] + 2].is_empty()
+        ):
+            grid[pos[0]][pos[1] + 1].reset()
+            return (pos[0], pos[1] + 2)
+        elif (
+            key == 4 and pos[0] > 2 and not grid[pos[0] - 2][pos[1]].is_empty()
+        ):
+            grid[pos[0] - 1][pos[1]].reset()
+            return (pos[0] - 2, pos[1])
         sides.remove(key)
         if not sides:
             return pos
